06e685904b8326aa93a0ba8c666f83b3bc49dacb
cesarvidarte <cesarvidarte666@hotmail.com>
2020-12-21 16:33:05 -0500

    Prototipar y personalizar vistas

--- angular-wishlist/src/app/models/destino-viaje.model.ts
+++ angular-wishlist/src/app/models/destino-viaje.model.ts
@@ -1,6 +1,8 @@
 export class DestinoViaje {
   private selected:boolean;
+  public servicios: string[];
   constructor(public nombre: string,public imagenUrl: string) {
+    this.servicios = ['pileta', 'desayuno'];
   }
   isSelected():boolean{
     return this.selected;

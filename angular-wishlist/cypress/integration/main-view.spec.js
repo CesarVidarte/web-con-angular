46d03af74286c5b8d523e4c6900f65a77c6b339b
cesarvidarte <cesarvidarte666@hotmail.com>
2020-12-29 19:30:21 -0500

    adición de 2 pruebas extra personalizadas

--- angular-wishlist/cypress/integration/main-view.spec.js
+++ angular-wishlist/cypress/integration/main-view.spec.js
@@ -1,7 +1,16 @@
 describe('ventana principal', () => {
+    it('tiene titulo correcto en el home', () => {
+        cy.visit('http://localhost:4200');
+        cy.contains('Ruteo simple');
+    });
+
     it('tiene encabezado correcto y en español por defecto', () => {
-      cy.visit('http://localhost:4200');
-      cy.contains('angular-wishlist');
-      cy.get('h1 b').should('contain', 'HOLA es');
+        cy.visit('http://localhost:4200');
+        cy.contains('angular-wishlist');
+        cy.get('h1 b').should('contain', 'HOLA es');
+    });
+    it('tiene footer correcto', () => {
+        cy.visit('http://localhost:4200');
+        cy.contains('Actividad');
     });
   });
